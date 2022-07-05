2aa1ec5bc72a45a6aa9821d43fa4fa5a2ac1d27f
Marcos Guibso <mgss.lic17@uea.edu.br>
2022-07-05 00:39:07 -0300

    feat: Construção do layout da aplicação

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+    return(
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Preço</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td className="deposit">R$1200,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$1.100,00</td>
+                        <td>Casa</td>
+                        <td>02/02/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #F0F2F5;
+        --red: #E52E4D;
+        --blue: #5429CC;
+        --green: #33cc95;
+
+        --blue-light: #6933ff;
+
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+
+        --shape: #ffff;
+
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media (max-width: 1080px){
+            font-size: 93.75%; //15px
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%; //14px
+        }
+    }
+
+    body{
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button{
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong{
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+
+`
